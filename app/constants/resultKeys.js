777d4414c48e4e9fe4e797e0a718bb3bcf1f1eea
Jason Zajdel <zajdel.jason@gmail.com>
2018-12-12 12:20:53 -0800

    Added constants file for result keys

---
+++ app/constants/resultKeys.js
@@ -0,0 +1,16 @@
+import { constants } from 'remediator';
+
+const {
+  RETURN_DATA_OUTPUT_KEY,
+  RETURN_DATA_SOURCE_KEY,
+  RETURN_DATA_ERROR_KEY,
+} = constants;
+
+/**
+ * Re-exporting remediator constants that remediator-cli cares about.
+ */
+export {
+  RETURN_DATA_OUTPUT_KEY,
+  RETURN_DATA_SOURCE_KEY,
+  RETURN_DATA_ERROR_KEY,
+};
